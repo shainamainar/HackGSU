48a62040aa2fa9087502e647267db34609f885bf
shainamainar <shainamainar@gmail.com>
2019-11-03 01:26:05 -0500

    update

--- flutter_app/ios/Runner/AppDelegate.swift
+++ flutter_app/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyB77LfxW3UAJO9Iq60TMRTc5VqHXsd4KT0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -31,8 +34,12 @@ class _MyHomePageState extends State<MyHomePage> {
     'Dry Rub': false,
     'Jerk': false,
   };
-  void locate(){
-    //TODO
+
+  void locate() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MapPage())
+    );
   }
 
   @override
@@ -40,45 +47,94 @@ class _MyHomePageState extends State<MyHomePage> {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Wing Drop',
-      home: Scaffold(
-        appBar: AppBar(centerTitle: true, title: Text('Wing Drop',), backgroundColor: Colors.amber,),
-        body: Align(
-          alignment: Alignment.center,
-            child: Column(
-              children: <Widget>[
-                Image.asset('assets/logo.png', width: 300.0, height: 200.0,),
-                Flexible(child: new ListView(
-                  children: values.keys.map((String key) {
-                    return new CheckboxListTile(
-                        activeColor: Colors.amber,
-                        title: new Text(key),
-                        value: values[key],
-                        onChanged: (bool value) {
-                          setState(() {
-                            values[key] = value;
-                    });
-                  },
-                );
-                  }).toList(),
-                ),
-                ),
-                Flexible(child: Align(
-                  child: RaisedButton(
-                  onPressed: locate,
-                  padding:  EdgeInsets.all(8.0),
-                  textColor: Colors.white,
-                  color: Colors.amber,
-                  child: Text("Locate"),
-                ),
-                )
-                )
-        ]
-      ),
-    )
-    )
+        home: Scaffold(
+            appBar: AppBar(centerTitle: true,
+              title: Text('Wing Drop',),
+              backgroundColor: Colors.amber,),
+            body: Align(
+              alignment: Alignment.center,
+              child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/logo.png', width: 300.0, height: 200.0,),
+                    Container(
+                      height: MediaQuery.of(context).copyWith().size.height/3,
+                      child: new ListView(
+                      children: values.keys.map((String key) {
+                        return new CheckboxListTile(
+                          activeColor: Colors.amber,
+                          title: new Text(key),
+                          value: values[key],
+                          onChanged: (bool value) {
+                            setState(() {
+                              values[key] = value;
+                            });
+                          },
+                        );
+                      }).toList(),
+                    ),
+                    ),
+                    Flexible(child: Align(
+                      alignment: Alignment(0.0, 0.7),
+                      child: RaisedButton(
+                        onPressed: locate,
+                        padding: EdgeInsets.all(8.0),
+                        textColor: Colors.white,
+                        color: Colors.amber,
+                        child: Text("Locate"),
+                      ),
+                    )
+                    ),
+
+                  ]
+              ),
+            )
+        )
     );
+  }
+}
+
+class MapPage extends StatefulWidget {
+  @override
+  State<MapPage> createState() => MapPageState();
+
+}
+class MapPageState extends State<MapPage>{
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _myLocation =
+  CameraPosition(target: LatLng(33.753478,-84.385239),);
+  @override
+  Widget build(BuildContext context){
+      return Scaffold(
+      appBar: AppBar(
+        title: Text('Locate'),
+
+      ),
+      body: Stack(
+        children: <Widget>[
+          _googleMap(context),
+          //_buildContainer(),
+        ],
+      )
+      );
+    }
+    Widget _googleMap(BuildContext context){
+      return Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: _myLocation,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+
+        )
+      );
+    }
   }
 
 
-}
+
+
 
